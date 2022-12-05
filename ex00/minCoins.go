851183b5f3690723b1bf073cfd85187e4de1d802
bemmanue <myaushenka@gmail.com>
2022-12-05 22:59:19 +0300

    ex00 is done

--- ex00/minCoins.go
+++ ex00/minCoins.go
@@ -1,5 +1,7 @@
 package main
 
+import "sort"
+
 func minCoins(val int, coins []int) []int {
 	res := make([]int, 0)
 	i := len(coins) - 1
@@ -11,4 +13,35 @@ func minCoins(val int, coins []int) []int {
 		i -= 1
 	}
 	return res
+}
+
+func minCoins2(val int, coins []int) []int {
+	coins = removeDuplicates(coins)
+	sort.Slice(coins, func(i, j int) bool {
+		return coins[i] < coins[i]
+	})
+
+	res := make([]int, 0)
+	i := len(coins) - 1
+	for i >= 0 {
+		for val >= coins[i] {
+			val -= coins[i]
+			res = append(res, coins[i])
+		}
+		i -= 1
+	}
+	return res
+}
+
+func removeDuplicates(array []int) []int {
+	allKeys := make(map[int]bool)
+	list := []int{}
+
+	for _, item := range array {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
 }
