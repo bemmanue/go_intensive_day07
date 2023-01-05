9eee466bd7a8ba7b73f7ee3d828d2e310a6cf222
Burma Emmanuel <bemmanue@si-g4.kzn.21-school.ru>
2023-01-05 22:36:13 +0300

    ?

--- ex01/minCoins.go
+++ ex01/minCoins.go
@@ -1,6 +1,9 @@
 package main
 
-import "sort"
+import (
+	"net/http"
+	_ "net/http/pprof"
+)
 
 func minCoins(val int, coins []int) []int {
 	res := make([]int, 0)
@@ -17,25 +20,47 @@ func minCoins(val int, coins []int) []int {
 
 func minCoins2(val int, coins []int) []int {
 	coins = removeDuplicates(coins)
-	sort.Slice(coins, func(i, j int) bool {
-		return coins[i] < coins[i]
-	})
 
-	res := make([]int, 0)
-	i := len(coins) - 1
-	for i >= 0 {
-		for val >= coins[i] {
-			val -= coins[i]
-			res = append(res, coins[i])
+	var memo = make([][]int, val+1)
+
+	for _, coin := range coins {
+		if coin <= val {
+			memo[coin] = []int{coin}
 		}
-		i -= 1
+		if coin == val {
+			return memo[coin]
+		}
 	}
-	return res
+
+outer:
+	for i := 2; i <= val; i++ {
+		for _, coin := range coins {
+			if i == coin {
+				continue outer
+			}
+		}
+
+		var mincoins = make(map[int][]int)
+
+		for _, coin := range coins {
+			if i > coin {
+				mincoins[coin] = append(memo[i-coin], coin)
+			}
+		}
+
+		m := minimum(mincoins)
+
+		minSet := make([]int, len(mincoins[m]))
+		copy(minSet, mincoins[m])
+
+		memo[i] = minSet
+	}
+	return memo[val]
 }
 
 func removeDuplicates(array []int) []int {
 	allKeys := make(map[int]bool)
-	list := []int{}
+	var list []int
 
 	for _, item := range array {
 		if _, value := allKeys[item]; !value {
@@ -44,4 +69,25 @@ func removeDuplicates(array []int) []int {
 		}
 	}
 	return list
+}
+
+func minimum(mincoins map[int][]int) int {
+	var min int
+
+	for i := range mincoins {
+		min = i
+		break
+	}
+
+	for m := range mincoins {
+		if len(mincoins[m]) < len(mincoins[min]) {
+			min = m
+		}
+	}
+
+	return min
+}
+
+func main() {
+	http.ListenAndServe("localhost:8080", nil)
 }
--- ex01/minCoins_test.go
+++ ex01/minCoins_test.go
@@ -1,63 +1,112 @@
 package main
 
 import (
-	"log"
+	"math"
+	"strconv"
 	"testing"
 )
 
-func TestFirst(t *testing.T) {
-	coins := []int{1, 5, 10, 50, 100, 1000}
-
-	result := minCoins2(1864, coins)
-
-	coinsMap := make(map[int]int)
-	for _, coin := range coins {
-		coinsMap[coin] = 0
-	}
-
-	for _, coin := range result {
-		if _, ok := coinsMap[coin]; !ok {
-			log.Fatalln("Test 1: result contains nonexistant coin")
-		}
-		coinsMap[coin]++
-	}
-
-	for i := 0; i+1 < len(coins); i++ {
-		if coinsMap[coins[i]] >= coins[i+1]/coins[i] {
-			log.Fatalln("Test 1: returned slice of coins not of minimal size")
-		}
-	}
-}
-
-func TestSecond(t *testing.T) {
-	coins := []int{1, 5, 10, 1, 50, 5, 100, 10, 1000}
-
-	result := minCoins2(1864, coins)
-
-	coins = []int{1, 5, 10, 50, 100, 1000}
-
-	coinsMap := make(map[int]int)
-	for _, coin := range coins {
-		coinsMap[coin] = 0
-	}
-
-	for _, coin := range result {
-		if _, ok := coinsMap[coin]; !ok {
-			log.Fatalln("Test 2: result contains nonexistant coin")
-		}
-		coinsMap[coin]++
-	}
-
-	for i := 0; i+1 < len(coins); i++ {
-		if coinsMap[coins[i]] >= coins[i+1]/coins[i] {
-			log.Fatalln("Test 2: returned slice of coins not of minimal size")
-		}
-	}
-}
+var value int = 61
 
 func BenchmarkMinCoins(b *testing.B) {
-	coins := []int{1, 5, 10, 50, 100, 1000}
-	for i := 0; i < b.N; i++ {
-		minCoins2(1864, coins)
+	coinsSet := [][]int{
+		{1, 5, 10, 50, 100, 500, 1000},
+		{1, 3, 4, 7, 13, 15},
+		{1000, 5, 10, 50, 100, 500, 1},
+		{15, 13, 7, 4, 3, 1},
 	}
+
+	for i, coins := range coinsSet {
+		b.Run("Test_"+strconv.Itoa(i+1), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				//minCoins(value, coins)
+				minCoins2(value, coins)
+			}
+		})
+	}
+}
+
+func TestMinCoins(t *testing.T) {
+	coinsSet := [][]int{
+		{1, 5, 10, 50, 100, 500, 1000},
+		{1, 3, 4, 7, 13, 15},
+		{1000, 5, 10, 50, 100, 500, 1},
+		{15, 13, 7, 4, 3, 1},
+	}
+
+	for i, coins := range coinsSet {
+		t.Run("Test_"+strconv.Itoa(i+1), func(t *testing.T) {
+			//result := minCoins(value, coins)
+			result := minCoins2(value, coins)
+
+			coinsMap := make(map[int]bool)
+			for _, coin := range coins {
+				coinsMap[coin] = true
+			}
+
+			var sum int
+			for _, coin := range result {
+				if _, ok := coinsMap[coin]; !ok {
+					t.Error("result contains nonexistant coin")
+				}
+				sum += coin
+			}
+
+			if len(result) != minCount(value, coins) {
+				t.Error("returned slice of coins not of minimal size")
+			}
+
+			if len(result) != 0 && sum != value {
+				t.Error("the sum of returned coins is not equal to expected value")
+			}
+		})
+	}
+}
+
+func minCount(val int, coins []int) int {
+	coins = removeDuplicates(coins)
+
+	var max = math.MaxInt
+	var memo = make([]int, val+1)
+
+	for i := range memo {
+		memo[i] = max
+	}
+
+	for _, coin := range coins {
+		if val > coin {
+			memo[coin] = 1
+		}
+	}
+
+outer:
+	for i := 2; i <= val; i++ {
+
+		for _, coin := range coins {
+			if i == coin {
+				continue outer
+			}
+		}
+
+		var mincoins = make([]int, len(coins))
+
+		for j, coin := range coins {
+			if i > coin {
+				mincoins[j] = memo[i-coin]
+			} else {
+				mincoins[j] = max
+			}
+		}
+
+		min := max
+
+		for j := range mincoins {
+			if mincoins[j] < min {
+				min = mincoins[j]
+			}
+		}
+
+		memo[i] = min + 1
+	}
+	return memo[val]
 }
